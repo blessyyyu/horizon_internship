cb76edad82acbb9c9819a4752f2e259ba3c47bb8
blessyyyu <954793264@qq.com>
2022-02-28 18:32:12 +0800

    change to google style

--- basic_programming/cplusplus_basic_programming/cplusplus_basic_programming/myvector.h
+++ basic_programming/cplusplus_basic_programming/cplusplus_basic_programming/myvector.h
@@ -5,89 +5,85 @@
 
 template<class T>
 class MyVector {
-public:
-	// 无参数的构造函数
-	MyVector() : size_(0), data_(nullptr), capacity_(this->kexpandsize_){}
-	// 带参数的构造函数
-	MyVector(int size, T data) {
-		// 在设定的空间上再多开辟一段空间
-		this->capacity_ = size + this->kexpandsize_;
-		this->size_ = size;
-		this->data_ = new T[size_];
-		for (int i = 0; i < this->size_; i++) {
-			this->data_[i] = data;
+ public:
+  // 无参数的构造函数
+  MyVector() : size_(0), data_(nullptr), capacity_(this->kexpandsize_){}
+  // 带参数的构造函数
+  MyVector(int size, T data) {
+    // 在设定的空间上再多开辟一段空间
+    this->capacity_ = size + this->kexpandsize_;
+    this->size_ = size;
+    this->data_ = new T[size_];
+    for (int i = 0; i < this->size_; i++) {
+      this->data_[i] = data;
+    }
+  }
+
+  // 复制构造函数
+  MyVector(const MyVector & other) {
+    this->size_ = other.size_;
+    this->data_ = new T[this->size_ + this->kexpandsize_];
+    for (int i = 0; i < this->size_; i++) {
+      this->data_[i] = other.data_[i];
+    }
+  }
+
+  //析构函数
+  ~MyVector() {
+    if (this->size_ != 0) {
+      delete[] this->data_;
+    }
+  }
+
+  //push_back函数
+  void push_back(T element) {
+    if (this->data_ == nullptr) {
+      this->size_ = 1;
+      this->data_ = new T[this->size_ + this->kexpandsize_];
+      data_[0] = element;
+    }
+    else{
+      if (this->size_ >= this->capacity_) {
+        T * temp = this->data_;			
+        this->capacity_ = 2 * this->capacity_ + 1;
+        this->data_ = new T[this->capacity_];
+        int index;
+		for (index = 0; index < this->size_; index++) {
+			this->data_[index] = temp[index];
 		}
+		this->data_[index] = element;
+		this->size_ += 1;
+		delete[] temp;
+      }
+	  else {
+	    this->data_[this->size_++] = element;
+	  }
 	}
+  }
 
-	// 复制构造函数
-	MyVector(const MyVector & other) {
-		this->size_ = other.size_;
-		this->data_ = new T[this->size_ + this->kexpandsize_];
-		for (int i = 0; i < this->size_; i++) {
-			this->data_[i] = other.data_[i];
-		}
+  T & operator[](int index) {
+    if(index >= 0 && index < this->size_)
+      return this->data_[index];
+  }
+
+  void clear() {
+    this->size_ = 0;
+  }
+
+  void print() {
+    for (int i = 0; i < this->size_; i++) {
+      std::cout << this->data_[i] << " ";
 	}
+	std::cout << std::endl;
+  }
 
-	//析构函数
-	~MyVector() {
-		if (this->size_ != 0) {
-			delete[] this->data_;
-		}
-	}
-
-	//push_back函数
-	void push_back(T element) {
-		if (this->data_ == nullptr) {
-			this->size_ = 1;
-			this->data_ = new T[this->size_ + this->kexpandsize_];
-			data_[0] = element;
-		}
-		else {
-			if (this->size_ >= this->capacity_) {
-				T * temp = this->data_;			
-				this->capacity_ = 2 * this->capacity_ + 1;
-				this->data_ = new T[this->capacity_];
-
-				int index;
-				for (index = 0; index < this->size_; index++) {
-					this->data_[index] = temp[index];
-				}
-				this->data_[index] = element;
-				this->size_ += 1;
-				delete[] temp;
-			}
-			else {
-				this->data_[this->size_++] = element;
-			}
-		}
-
-
-	}
-
-	T & operator[](int index) {
-		if(index >= 0 && index < this->size_)
-			return this->data_[index];
-	}
-
-	void clear() {
-		this->size_ = 0;
-	}
-
-	void print() {
-		for (int i = 0; i < this->size_; i++) {
-			std::cout << this->data_[i] << " ";
-		}
-		std::cout << std::endl;
-	}
-
-
-private:
-	const int kexpandsize_ = 32;
-	// 当前的空间
-	int size_;
-	// 实际开辟的容量
-	int capacity_;
-	T * data_;
+ private:
+  const int kexpandsize_ = 32;
+  // 当前的空间
+  int size_;
+  // 实际开辟的容量
+  int capacity_;
+  T * data_;
 };
 
 
--- basic_programming/cplusplus_basic_programming/list实现/mylist.h
+++ basic_programming/cplusplus_basic_programming/list实现/mylist.h
@@ -1,86 +1,79 @@
 ﻿#ifndef MY_LIST_H
 #define MY_LIST_H
 
+#include <iostream>
+#include <string>
+#include <variant>
+#include <vector>
 
-#include<vector>
-#include<variant>
-#include<iostream>
-#include<string>
-using T = std::variant<std::string, const char*, bool, char, unsigned char, int, size_t, float, double>;
-
+using T = std::variant<std::string, const char*, bool, char, unsigned char, int,
+                       size_t, float, double>;
 
 class MyList {
-public:
-	// 默认的构造函数
-	MyList() {
-		std::vector<T> list_;
-	}
-	// 带参数的构造函数
-	MyList(const T & var) {
-		this->list_.push_back(var);
-	}
-	// 带多个参数的构造函数
-	MyList(const std::initializer_list<T> & lst) {
-		this->list_.resize(lst.size());
-		std::copy(lst.begin(), lst.end(), this->list_.begin());
-	}
-	
-	~MyList(){}
-	// append函数以及它的重载
-	void append(const T & var) {
-		this->list_.push_back(var);
-	}
+ public:
+  // 默认的构造函数
+  MyList() { std::vector<T> list_; }
+  // 带参数的构造函数
+  MyList(const T& var) { this->list_.push_back(var); }
+  // 带多个参数的构造函数
+  MyList(const std::initializer_list<T>& lst) {
+    this->list_.resize(lst.size());
+    std::copy(lst.begin(), lst.end(), this->list_.begin());
+  }
 
-	void append(const std::vector<T>&  values) {
-		for (auto i = values.begin(); i != values.end(); i ++) {
-			this->list_.push_back(*i);
-		}
-	}
+  ~MyList() {}
+  // append函数以及它的重载
+  void append(const T& var) { this->list_.push_back(var); }
 
-	void append(const std::initializer_list<T> & values) {
-		for (auto i = values.begin(); i != values.end(); i ++) {
-			this->list_.push_back(*i);
-		}
-	}
+  void append(const std::vector<T>& values) {
+    for (auto i = values.begin(); i != values.end(); i++) {
+      this->list_.push_back(*i);
+    }
+  }
 
-	// 单个元素做类型推断再输出
-	void base_print(const T & var) {
-		std::visit([&](const auto& arg) {
-			using Ttype = typename std::decay<decltype(arg)>::type;
-			using Ctype = typename std::decay<const char*>::type;
-			constexpr bool b = std::is_same_v<Ttype, std::string>;
-			// 如果推断出来是string或者const char* 类型
-			if constexpr (b || std::is_same_v<Ttype, Ctype>) {
-				auto s = std::string(arg);
-				if (s.size())
-					std::cout << "'" << s << "'";
-			}
-			else {
-				// 否则可以直接输出
-				std::cout << arg;
-			}
-		}, var);
-	}
+  void append(const std::initializer_list<T>& values) {
+    for (auto i = values.begin(); i != values.end(); i++) {
+      this->list_.push_back(*i);
+    }
+  }
 
-	void print_vector() {
-		std::cout << "[";
-		int n = this->list_.size(); int i = 0;
+  // 单个元素做类型推断再输出
+  void base_print(const T& var) {
+    std::visit(
+        [&](const auto& arg) {
+          using Ttype = typename std::decay<decltype(arg)>::type;
+          using Ctype = typename std::decay<const char*>::type;
+          constexpr bool b = std::is_same_v<Ttype, std::string>;
+          // 如果推断出来是string或者const char* 类型
+          if constexpr (b || std::is_same_v<Ttype, Ctype>) {
+            auto s = std::string(arg);
+            if (s.size()) std::cout << "'" << s << "'";
+          } else {
+            // 否则可以直接输出
+            std::cout << arg;
+          }
+        },
+        var);
+  }
 
-		for (auto it = this->list_.begin(); it != this->list_.end(); it++, i++) {
-			std::visit([&](const auto& arg) {
-				base_print(arg);
-				if (i < n - 1)
-					std::cout << ",";
-			}, *it);
-		}
-		std::cout << "]" << std::endl;
-	}
+  void print_vector() {
+    std::cout << "[";
+    int n = this->list_.size();
+    int i = 0;
 
+    for (auto it = this->list_.begin(); it != this->list_.end(); it++, i++) {
+      std::visit(
+          [&](const auto& arg) {
+            base_print(arg);
+            if (i < n - 1) std::cout << ",";
+          },
+          *it);
+    }
+    std::cout << "]" << std::endl;
+  }
 
-
-private:
-	std::vector<T> list_;
+ private:
+  std::vector<T> list_;
 };
 
-
-#endif // !MY_LIST_H
+#endif  // !MY_LIST_H
